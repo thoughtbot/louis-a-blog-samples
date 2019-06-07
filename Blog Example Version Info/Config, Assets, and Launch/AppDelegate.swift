eacf341993dc6bae7b9813fb5170f192008cb118
Louis Antonopoulos <louis@thoughtbot.com>
2019-06-07 17:56:50 -0400

    Log version info on app launch

--- Blog Example Version Info/Config, Assets, and Launch/AppDelegate.swift
+++ Blog Example Version Info/Config, Assets, and Launch/AppDelegate.swift
@@ -5,6 +5,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    print("\(Bundle.main.bundleID) - \(Bundle.main.versionString)")
     return true
   }
 
