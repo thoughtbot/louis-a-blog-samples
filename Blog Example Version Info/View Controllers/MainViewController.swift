045e9e6bd77619a48a4b0b03ab7cb3b34de1c099
Louis Antonopoulos <louis@thoughtbot.com>
2019-06-14 17:32:23 -0400

    Add text view and info button to Main.storyboard

--- Blog Example Version Info/View Controllers/MainViewController.swift
+++ Blog Example Version Info/View Controllers/MainViewController.swift
@@ -1,6 +1,11 @@
 import UIKit
 
 class MainViewController: UIViewController {
+  @IBAction func infoButtonTouchUpInside(_ sender: Any) {
+    showOkAlert(title: "Info",
+                message: Bundle.main.versionString)
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
   }
